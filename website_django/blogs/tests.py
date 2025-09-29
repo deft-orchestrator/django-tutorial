0aeb36410daf2f1e989b0b9900e611fa830debb0
Deft Orchestrator <143682155+deft-orchestrator@users.noreply.github.com>
2025-09-29 02:31:09 +0000

    feat: Enhance blog application with detailed README, pagination, and styling

--- website_django/blogs/tests.py
+++ website_django/blogs/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import Post
+
+class PostModelTest(TestCase):
+	def test_create_post(self):
+		post = Post.objects.create(title="Judul", content="Isi konten")
+		self.assertEqual(str(post), "Judul")
+
+class BlogViewsTest(TestCase):
+	def setUp(self):
+		self.post = Post.objects.create(title="Judul", content="Isi konten")
+		self.client = Client()
+
+	def test_post_list_view(self):
+		response = self.client.get(reverse('post_list'))
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, self.post.title)
+
+	def test_post_detail_view(self):
+		response = self.client.get(reverse('post_detail', args=[self.post.id]))
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, self.post.title)
+
