f7641f57ca122ff40c7a44f737137dcd5f4d25e0
Deft Orchestrator <143682155+deft-orchestrator@users.noreply.github.com>
2025-09-29 04:54:38 +0000

    Bersihkan branch part-2: hanya setup app blogs dan pendaftaran di settings, tanpa model dan migrasi

--- website_django/blogs/models.py
+++ website_django/blogs/models.py
@@ -1,12 +1,3 @@
 from django.db import models
 
 # Create your models here.
-from django.utils import timezone
-
-class Post(models.Model):
-	title = models.CharField(max_length=200)
-	content = models.TextField()
-	published_date = models.DateTimeField(default=timezone.now)
-
-	def __str__(self):
-		return str(self.title)
